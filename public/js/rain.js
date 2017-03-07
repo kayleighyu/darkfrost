f945a7eec537cdcc260ed32ace1619e1b4eb6fd0
Kayleigh Yu <kayleighyu@gmail.com>
2017-03-06 21:07:46 -0500

    added rain.js code

---
+++ public/js/rain.js
@@ -0,0 +1,25 @@
+// number of drops created.
+var nbDrop = 90;
+
+// function to generate a random number range.
+function randRange( minNum, maxNum) {
+  return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
+}
+
+// function to generate drops
+function createRain() {
+
+	for( i=1;i<nbDrop;i++) {
+	var dropLeft = randRange(0,1600);
+	var dropTop = randRange(-1000,1400);
+
+	$('.rain').append('<div class="drop" id="drop'+i+'"></div>');
+	$('#drop'+i).css('left',dropLeft);
+	$('#drop'+i).css('top',dropTop);
+	}
+
+}
+// Make it rain
+if(document.querySelector(".background-image").getAttribute("src") == '/images/clear-night.jpg'){
+   createRain();
+ }
